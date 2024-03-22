5060b345d87af08c690db681781dcc889e365cb3
p1yu5h0 <piyush200205@gmail.com>
2024-03-22 12:52:21 +0530

    video-server updated

--- server/src/video-server.js
+++ server/src/video-server.js
@@ -3,37 +3,44 @@ const fs = require("fs");
 const path = require("path");
 
 const port = 4001;
-const publicDirectory = "./uploads/processed/hls";
+const publicDirectory = "/Users/piyushagrawal/projects/video-sharing/server/uploads/processed/hls/";
 
 const requestHandler = (req, res) => {
-  const filePath = path.join(publicDirectory, req.url);
-  console.log("filePath", filePath);
-
-  fs.stat(filePath, (err, stats) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end(`Error reading file: ${err}`);
-      return;
-    }
-
-    if (stats.isDirectory()) {
-      res.statusCode = 403; // Forbidden
-      res.end(`Directory access is forbidden: ${filePath}`);
-      return;
-    }
-
-    fs.readFile(filePath, (err, data) => {
+    const filePath = path.join(publicDirectory, req.url);
+    console.log("filePath", filePath);
+  
+    fs.stat(filePath, (err, stats) => {
       if (err) {
         res.statusCode = 500;
         res.end(`Error reading file: ${err}`);
         return;
       }
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-      res.setHeader("Access-Control-Max-Age", 2592000); 
-      res.end(data);
+  
+      if (stats.isDirectory()) {
+        fs.readdir(filePath, (err, files) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(`Error reading directory: ${err}`);
+            return;
+          }
+          res.setHeader("Content-Type", "text/html");
+          const fileList = files.map(file => `<li><a href="${req.url}/${file}">${file}</a></li>`).join('');
+          res.end(`<h1>Directory Listing:</h1><ul>${fileList}</ul>`);
+        });
+      } else {
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(`Error reading file: ${err}`);
+            return;
+          }
+          res.setHeader("Access-Control-Allow-Origin", "*");
+          res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+          res.setHeader("Access-Control-Max-Age", 2592000); 
+          res.end(data);
+        });
+      }
     });
-  });
 };
 
 const server = http.createServer(requestHandler);

5b6071659b8446bf3ffc12f4ee4fb8ef3bd6a3f9
p1yu5h0 <piyush200205@gmail.com>
2024-03-22 17:19:58 +0530

    hls working now

--- server/src/modules/queues/video-processor.js
+++ server/src/modules/queues/video-processor.js
@@ -1,39 +1,81 @@
 const ffmpeg = require("fluent-ffmpeg");
-const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
 const path = require("path");
-const fs = require("fs");
+const fs = require('fs')
 const { VIDEO_QUEUE_EVENTS: QUEUE_EVENTS } = require("./constants");
 const { addQueueItem } = require("./queue");
 
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
-console.log(ffmpegInstaller.path, ffmpegInstaller.version);
 
 const executeToMp4 = async (filePath, outputFolder, jobData) => {
+  // const fileName = path.basename(filePath);
+  // const extension = path.extname(filePath);
+  // const fileNameWithoutExt = path.basename(filePath, extension);
+
+  // const resolutions = ['1280x720', '854x480', '480x360'];
+  // const outputFiles = [];
+
+  // for (const resolution of resolutions) {
+  //   const subFolder = `${outputFolder}/${fileNameWithoutExt}`;
+  //   if (!fs.existsSync(subFolder)) {
+  //     fs.mkdirSync(subFolder);
+  //   }
+
+  //   const outputFileName = `${subFolder}/mp4-${resolution}.mp4`;
+
+  //   const command = ffmpeg(filePath)
+  //     .output(outputFileName)
+  //     .size(resolution)
+  //     .on("start", function (commandLine) {
+  //       console.log(`Spawned Ffmpeg with command for ${resolution}: ${commandLine}`);
+  //     })
+  //     .on("progress", function (progress) {
+  //       if (parseInt(progress.percent) % 10 === 0) {
+  //         console.log("Processing: " + progress.percent + "% done");
+  //       }
+  //     })
+  //     .on("end", function () {
+  //       console.log(`Finished processing ${resolution, outputFileName}`);
+  //       addQueueItem(QUEUE_EVENTS.VIDEO_PROCESSED, { ...jobData, completed: true,
+  //         path: outputFileName, });
+  //     })
+  //     .on("error", function (err) {
+  //       console.log(`An error occurred for ${resolution}: ${err.message}`);
+  //     })
+  //     .run();
+
+  //   outputFiles.push(outputFileName);
+  // }
+
+  // return;
+
   const fileExt = path.extname(filePath);
   const fileNameWithoutExt = path.basename(filePath, fileExt);
 
   const outputFileName = `${outputFolder}/${fileNameWithoutExt}.mp4`;
 
-  const uploadParams = {
-    Bucket: "video-piyush-s3",
-    Key: outputFileName,
-    Body: ffmpeg(filePath).format("mp4").pipe(),
-  };
+  ffmpeg(filePath)
+    .output(outputFileName)
+    .on("start", function (commandLine) {
+      console.log("Spawned Ffmpeg with command: " + commandLine);
+    })
+    .on("progress", function (progress) {
+      if (parseInt(progress.percent) % 20 === 0) {
+        console.log("Processing: " + progress.percent + "% done");
+      }
+    })
+    .on("end", async function () {
+      console.log("Finished processing", outputFileName);
+      await addQueueItem(QUEUE_EVENTS.VIDEO_PROCESSED, {
+        ...jobData,
+        completed: true,
+        path: outputFileName,
+      });
+    })
+    .on("error", function (err) {
+      console.log("An error occurred: " + err.message);
+    })
+    .run();
 
-  try {
-    await s3.upload(uploadParams).promise();
-    console.log("File uploaded successfully to S3:", outputFileName);
-    await addQueueItem(QUEUE_EVENTS.VIDEO_PROCESSED, {
-      ...jobData,
-      completed: true,
-      path: outputFileName,
-    });
-  } catch (error) {
-    console.error("Error uploading file to S3:", error);
-  }
+  return;
 };
 
 const executeMp4ToHls = async (filePath, outputFolder, jobData) => {
@@ -42,25 +84,38 @@ const executeMp4ToHls = async (filePath, outputFolder, jobData) => {
 
   const outputFileName = `${outputFolder}/${fileNameWithoutExt}.m3u8`;
 
-  const uploadParams = {
-    Bucket: "video-piyush-s3",
-    Key: outputFileName,
-    Body: ffmpeg(filePath).format("hls").pipe(),
-  };
+  ffmpeg(filePath)
+    .output(outputFileName)
+    .outputOptions([
+      "-hls_time 10",
+      "-hls_list_size 0",
+      "-hls_flags delete_segments",
+      "-hls_segment_filename",
+      `${outputFolder}/${fileNameWithoutExt}_%03d.ts`,
+    ])
+    .on("start", function (commandLine) {
+      console.log("Spawned Ffmpeg with command: " + commandLine);
+    })
+    .on("progress", function (progress) {
+      if (parseInt(progress.percent) % 20 === 0) {
+        console.log("Processing: " + progress.percent + "% done");
+      }
+    })
+    .on("end", function () {
+      console.log("Finished processing", outputFileName);
+      addQueueItem(QUEUE_EVENTS.VIDEO_HLS_CONVERTED, {
+        ...jobData,
+        path: outputFileName,
+      });
+    })
+    .on("error", function (err) {
+      console.log("An error occurred: " + err.message);
+    })
+    .run();
 
-  try {
-    await s3.upload(uploadParams).promise();
-    console.log("File uploaded successfully to S3:", outputFileName);
-    await addQueueItem(QUEUE_EVENTS.VIDEO_HLS_CONVERTED, {
-      ...jobData,
-      path: outputFileName,
-    });
-  } catch (error) {
-    console.error("Error uploading file to S3:", error);
-  }
+  return;
 };
 
 module.exports = {
-  executeToMp4,
-  executeMp4ToHls,
+  executeToMp4, executeMp4ToHls
 };
